fcc5eaec7080f91f42298fd0119359cd221e03d3
Starswheat <32076550+Starswheat@users.noreply.github.com>
2017-10-16 12:54:46 -0700

    Create name.js

---
+++ name.js
@@ -0,0 +1,13 @@
+function thatWas Easy（){
+   alert("That was easy!);
+}
+
+$(#easy').on("clicl", thatWasEasy);
+
+$(document)(.keypress(delegateKeypress);
+
+function delegateKeypress(event){
+  if(event.keyCode == 32){
+     $("#easy").trigger("click");
+     }
+}
